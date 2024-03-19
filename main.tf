e28eda669cf1150caddf906c9ff0ea504a5b09c1
omric <omric@jfrog.com>
2024-03-19 12:43:13 +0200

    add test module

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "remote" {
+     hostname = "tflocal.com"
+     organization = "terraform-backend-l4ud2izqug"
+     workspaces {
+         prefix = "jfrog-"
+     }
+  }
+}
+resource "null_resource" "test" {
+  count = 1
+  triggers = {
+    test0 = "dummydummydummydummydummydummydummydummydummydummydummydummydummydummydummydummydummydummydummydummydummy"
+  }
+}
