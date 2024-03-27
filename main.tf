71c871d4c5c9b7e8f5ba6cd20ed11939a41281c2
omric <omric@jfrog.com>
2024-03-27 12:07:49 +0200

    add 16

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "remote" {
-     hostname = "tflocal.com"
-     organization = "terraform-backend-l4ud2izqug"
-     workspaces {
-         prefix = "jfrog-"
-     }
-  }
-}
 resource "null_resource" "test" {
   count = 1
   triggers = {
